e16a7b5f93991e990a6325ead962de834b8a92a7
MQzaih <masaqzaih@gmail.com>
2020-12-10 03:04:05 +0200

    Initial Commit

---
+++ CoffeeMachineTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoffeeMachineTask
+//
+//  Created by Asal 2 on 10/12/2020.
+//  Copyright © 2020 Asal 2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
