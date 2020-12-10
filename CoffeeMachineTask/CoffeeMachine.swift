cb640bb1927487723a9b96d90be22d963a257574
MQzaih <masaqzaih@gmail.com>
2020-12-10 03:15:14 +0200

    hi

---
+++ CoffeeMachineTask/CoffeeMachine.swift
@@ -0,0 +1,9 @@
+//
+//  CoffeeMachine.swift
+//  CoffeeMachineTask
+//
+//  Created by Asal 2 on 10/12/2020.
+//  Copyright © 2020 Asal 2. All rights reserved.
+//
+
+import Foundation
---
+++ CoffeeMachineTask/Resources.swift
@@ -0,0 +1,9 @@
+//
+//  Resources.swift
+//  CoffeeMachineTask
+//
+//  Created by Asal 2 on 10/12/2020.
+//  Copyright © 2020 Asal 2. All rights reserved.
+//
+
+import Foundation
